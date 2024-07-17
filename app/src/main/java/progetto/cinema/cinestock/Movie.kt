fe0bca2703dde14e4d3a6d4680763235012a5b9e
marti <martina.proietti2@studio.unibo.it>
2024-07-17 11:21:50 +0200

    film added

--- app/src/main/java/progetto/cinema/cinestock/Movie.kt
+++ app/src/main/java/progetto/cinema/cinestock/Movie.kt
@@ -1,3 +1,9 @@
 package progetto.cinema.cinestock
 
-data class Movie(val id: Int, val title: String, val description: String, val price: Double, val imageResId: Int)
+data class Movie(
+    val id: Int,
+    val title: String,
+    val description: String,
+    val price: Double,
+    val imageResId: Int
+)
--- app/src/main/java/progetto/cinema/cinestock/MovieRepository.kt
+++ app/src/main/java/progetto/cinema/cinestock/MovieRepository.kt
@@ -2,7 +2,8 @@ package progetto.cinema.cinestock
 
 object MovieRepository {
     private val movies = listOf(
-        Movie(1, "Pinocchio", "Pinocchio, un burattino di legno intagliato da Geppetto e trasformato in una marionetta vivente", 10.0, R.drawable.pinocchio)
+        Movie(1, "Pinocchio", "Pinocchio, un burattino di legno intagliato da Geppetto e trasformato in una marionetta vivente", 10.0, R.drawable.pinocchio),
+        Movie(2, "Madagascar", "Quattro amici animali vivono tutti insieme in uno zoo, ma si troveranno a vivere una nuova avventura", 10.0, R.drawable.madagascar)
         // Aggiungi altri film se necessario
     )
 
